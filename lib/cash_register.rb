f70ac974a12a3a603bcb3bd4443849417f9d93a8
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-16 16:14:55 -0400

    commit tree 9020c778441f4acd911a78955952e40e1a0a0ef4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+class CashRegister
+    attr_accessor :total, :items
+    attr_reader :discount
 
+    def initialize (discount=0)
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_transactions = []
+    end
+    def add_item(title,price,quantity=1)
+        @total += price * quantity
+        @last_transactions.push([title, price, quantity])
+        quantity.times { @items.push(title) }
+    end
+    def apply_discount
+        if @discount != 0
+            @total = @total * (100-@discount) / 100
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    def void_last_transaction
+        if @last_transactions.length == 0
+            "Nothing in cart"
+        else
+            last = @last_transactions.pop
+            last[2].times do 
+                @total -= last[1]
+                @items.pop
+            end
+        end
+    end
+end
